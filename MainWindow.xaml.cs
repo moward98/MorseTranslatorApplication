c6ed53cd9cc10ee4e0394279a39e8dc645cbff1a
mward98 <87323945+mward98@users.noreply.github.com>
2022-06-29 10:59:31 -0400

    1.00 Basic GUI Working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MorseTranslator;
 using System.Timers;
+using System.Diagnostics;
+//using Utilities;
+//using System.Windows.Forms;
 
 
 namespace MorseTranslatorApplication
@@ -25,54 +28,113 @@ namespace MorseTranslatorApplication
     public partial class MainWindow : Window
     {
 
-        Transmitter morseTx = new Transmitter();
+        private Transmitter _morseTx = new Transmitter();
+
+        private Stopwatch _pressedStopwatch = new Stopwatch();
+
+        private Stopwatch releasedStopwatch = new Stopwatch();
+
+        private float _timeReleased = 0F;
+        private float _timePressed = 0F;
+
+        private bool _morseInputEnabled = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            
         }
 
         private void TextToMorse_Click(object sender, RoutedEventArgs e)
         {
+            InputInstr.Text = "Enter Text To Translate to Morse Below:";
             InputInstr.Visibility = Visibility.Visible;
             InputTextBorder.Visibility = Visibility.Visible;
             PrintMorseButton.Visibility = Visibility.Visible;
             PlayMorseButton.Visibility = Visibility.Visible;
+
+            _morseInputEnabled = false;
         }
+
         private void MorseToText_Click(object sender, RoutedEventArgs e)
         {
-            InputInstr.Visibility = Visibility.Hidden;
+            InputInstr.Text = "Click and tap M key on keyboard to input morse";
+            MorseInField.Visibility = Visibility.Visible;
+            InputInstr.Visibility = Visibility.Visible;
             InputTextBorder.Visibility = Visibility.Hidden;
             PrintMorseButton.Visibility = Visibility.Hidden;
             PlayMorseButton.Visibility = Visibility.Hidden;
+
+            _morseInputEnabled = true;
         }
+
         private void PrintMorse_Click(object sender, RoutedEventArgs e)
         {
             // Get and Save User Input
-            morseTx.OpInput = InputRequest();
+            _morseTx.OpInput = InputRequest();
 
             PrintMorseButton.IsEnabled = false;           
             PlayMorseButton.IsEnabled = false;           
-            Console.WriteLine("Morse to Text was selected");
             MorseOutputLabel.Text = null;
             PrintMorse();
         }
+
         private void PlayMorse_Click(object sender, RoutedEventArgs e)
         {
             // Get and Save User Input
-            morseTx.OpInput = InputRequest();
+            _morseTx.OpInput = InputRequest();
 
             PrintMorseButton.IsEnabled = false;
             PlayMorseButton.IsEnabled = false;
 
             PlayMorse();
-            
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.M && _morseInputEnabled)
+            {
+                if (releasedStopwatch.IsRunning)
+                {
+                    releasedStopwatch.Stop();
+                    _timeReleased = releasedStopwatch.ElapsedMilliseconds;
+                    releasedStopwatch.Reset();
+                }
+
+                if (!_pressedStopwatch.IsRunning)
+                {
+                    _pressedStopwatch.Start();
+                }
+
+            }
+        }
+
+        private void OnKeyUpHandler(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.M && _morseInputEnabled)
+            {
+                if (_pressedStopwatch.IsRunning)
+                {
+                    _pressedStopwatch.Stop();
+                    _timePressed = _pressedStopwatch.ElapsedMilliseconds;
+                    _pressedStopwatch.Reset();
+                }
+
+                if (!releasedStopwatch.IsRunning)
+                {
+                    releasedStopwatch.Start();
+                }
+            }
         }
 
         private void PlayMorse()
         {
-            morseTx.TransmitSoundMessage();
+            Thread morseDisplaythread = new Thread(() => SoundOutput());
+            morseDisplaythread.Start();
+        }
+
+        private void SoundOutput()
+        {
+            _morseTx.TransmitSoundMessage();
         }
 
         public string InputRequest()
@@ -84,7 +146,7 @@ namespace MorseTranslatorApplication
         private void PrintMorse()
         {
             // Call Transmitter to Transmit 
-            MorseOutputText morseTranslation = morseTx.TransmitTextMessage();
+            MorseOutputText morseTranslation = _morseTx.TransmitTextMessage();
 
             Thread morseDisplaythread = new Thread(() => RefreshOutput(morseTranslation));
             morseDisplaythread.Start();
